fd243bafe21efc1d9838bd99ef8bd4c9f68520a7
Andy2020code <144275177+Andy2020code@users.noreply.github.com>
2024-04-21 12:25:14 -0400

    pricing design

---
+++ server/main/static/main/js/pricing.js
@@ -0,0 +1,26 @@
+function toggleDiv() {
+  var content = document.getElementById("content");
+  if (content.style.display === "none" || content.style.display === "") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
+
+function toggleDiv02() {
+  var content = document.getElementById("content-01");
+  if (content.style.display === "none" || content.style.display === "") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
+
+function toggleDiv03() {
+  var content = document.getElementById("content-02");
+  if (content.style.display === "none" || content.style.display === "") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
